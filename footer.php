857fe2883e399294cc739653c43a87c641b30cf2
mantrabrain <mantrabrain@gmail.com>
2019-06-18 21:52:11 +0545

    update

--- footer.php
+++ footer.php
@@ -11,46 +11,48 @@
 
 ?>
 
-	</div><!-- #content -->
+</div><!-- #content -->
 
-	<footer id="colophon" class="site-footer">
-	<?php 
-		if ( true == get_theme_mod( 'november_zero_footer_section_ebable', true ) ) :
+<footer id="colophon" class="site-footer">
+    <?php
+    if (true == get_theme_mod('november_zero_footer_section_ebable', true)) :
 
-		$widget_areas = esc_attr( get_theme_mod('november_zero_footer_section_col') );
-		if( ! $widget_areas ){
-			$widget_areas = 4;
-		}
-		if ($widget_areas == 3) {
-			$cols = '3';
-		} elseif ($widget_areas == 4 ) {
-			$cols = '4';
-		} elseif ($widget_areas == 2) {
-			$cols = '2';
-		} elseif ($widget_areas == 1) {
-			$cols = '1';
-		}
-	?>
-		<div class="footer-top">
-			<div class="container">
-				<div class="grid col-<?php echo esc_attr($cols); ?>">
+        $widget_areas = esc_attr(get_theme_mod('november_zero_footer_section_col'));
+        if (!$widget_areas) {
+            $widget_areas = 4;
+        }
+        if ($widget_areas == 3) {
+            $cols = '3';
+        } elseif ($widget_areas == 4) {
+            $cols = '4';
+        } elseif ($widget_areas == 2) {
+            $cols = '2';
+        } elseif ($widget_areas == 1) {
+            $cols = '1';
+        }
+        ?>
+        <div class="footer-top">
+            <div class="container">
+                <div class="grid col-<?php echo esc_attr($cols); ?>">
 
-					<?php get_template_part( 'template-parts/footer' , 'sidebar' ); ?>
-				
-			</div>
-		
-		</div>
-	</div>
-	<?php 
-		endif;
-	?>
-		
-	<div class="site-info">
-			<?php  echo esc_html(get_theme_mod('november_zero_footer_copyright')); ?>
-			<span class="sep"> | </span>
-				 <?php  echo sprintf( esc_html__( 'Powered by %s', 'november-zero' ), '<a target="_blank" rel="designer" href="https://mantrabrain.com/">' . esc_html__( 'Mantrabrain', 'november-zero' ) . '</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
+                    <?php get_template_part('template-parts/footer', 'sidebar'); ?>
+
+                </div>
+
+            </div>
+        </div>
+    <?php
+    endif;
+    ?>
+
+    <div class="site-info">
+        <div class="container">
+            <?php echo esc_html(get_theme_mod('november_zero_footer_copyright')); ?>
+            <span class="sep"> | </span>
+            <?php echo sprintf(esc_html__('Powered by %s', 'november-zero'), '<a target="_blank" rel="designer" href="https://mantrabrain.com/">' . esc_html__('Mantrabrain', 'november-zero') . '</a>'); ?>
+        </div>
+    </div><!-- .site-info -->
+</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
